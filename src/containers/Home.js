dfcce493348ba9abd221a34ce46c2c647947510c
Mikaayel Rehman <mika@Mikaayels-iMac.local>
2018-04-14 12:13:22 +0100

    added dist, updated eslint, web worker file

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { withSiteData } from 'react-static'
 //
-import logoImg from '../logo.png'
+// import logoImg from '../logo.png'
 
-export default withSiteData(() => (
-  <div>
-    <h1 style={{ textAlign: 'center' }}>Welcome to</h1>
-    <img src={logoImg} alt="" />
-  </div>
-))
+// export default withSiteData(() => (
+//   <div>
+//     <h1 style={{ textAlign: 'center' }}>Welcome to</h1>
+//     <img src={logoImg} alt="" />
+//   </div>
+// ))
+
+class Home extends Component {
+	constructor(props) {
+		super(props);
+		if(window.Worker) {
+			console.log('worker supported');
+		}	
+
+	}
+	render() {
+		return (
+			<div>
+				<h1>Home</h1>
+			</div>
+		);
+	}
+}
+
+export default withSiteData(Home);
